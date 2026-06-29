12f1ad37fd4642eddb43dde0f30b0d6b50acb2d0
Prince kumar <122259133+prince093kumar@users.noreply.github.com>
2026-06-29 16:12:07 +0530

    1143-longest-common-subsequence: accepted in Java | Time 22 ms | Memory 53.9 MB

---
+++ 1143-longest-common-subsequence/README.md
@@ -0,0 +1,6 @@
+# Longest Common Subsequence
+
+Difficulty: Medium  
+Language: Java  
+Submitted: 2026-06-29T10:41:59.000Z  
+LeetCode: https://leetcode.com/problems/longest-common-subsequence/
---
+++ 1143-longest-common-subsequence/longest-common-subsequence.java
@@ -0,0 +1,66 @@
+class Solution {
+    public int longestCommonSubsequence(String text1, String text2) {
+        int m=text1.length();
+        int n=text2.length();
+        
+        // recursion
+        // int i=m-1;
+        // int j=n-1;
+        // int[][] dp=new int[m][n];
+        // for(int k=0;k<m;k++){
+        //     Arrays.fill(dp[k],-1);
+        // }
+        // return lcs(text1,text2,i,j,dp);
+
+        // using dp 
+        int[][] dp=new int[m][n];
+        return lcs(text1,text2,m,n,dp);
+    }
+
+    // using dp with recursion 
+    // public int lcs(String s,String t,int i,int j,int[][] dp){
+    //     if(i<0 || j<0){
+    //         return 0;
+    //     }
+
+    //     if(dp[i][j]!=-1){
+    //         return dp[i][j];
+    //     }
+
+    //     if(s.charAt(i)==t.charAt(j)){
+    //         return dp[i][j]=lcs(s,t,i-1,j-1,dp)+1;
+    //     }
+    //     else{
+    //         return dp[i][j]=0+Math.max(lcs(s,t,i-1,j,dp),lcs(s,t,i,j-1,dp));
+    //     }
+    // }
+
+    // using dp without recursion 
+    public int lcs(String s,String t,int m,int n,int[][] dp){
+
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                
+                if(s.charAt(i)==t.charAt(j)){
+                    int a=0;
+                    if(i>0 && j>0){
+                        a=dp[i-1][j-1];
+                    }
+                    dp[i][j]=1+a;
+                }
+                else{
+                    int b=0;
+                    int c=0;
+                    if(i>0){
+                        b=dp[i-1][j];
+                    }
+                    if(j>0){
+                        c=dp[i][j-1];
+                    }
+                    dp[i][j]=0+Math.max(b,c);
+                }
+            }
+        }
+        return dp[m-1][n-1];
+    }
+}
