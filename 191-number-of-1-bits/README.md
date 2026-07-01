197921cc7ae23d8c700d27787bdb41a3306fadd2
Prince kumar <122259133+prince093kumar@users.noreply.github.com>
2026-07-01 16:39:56 +0530

    191-number-of-1-bits: accepted in Java | Time 1 ms | Memory 42.4 MB

---
+++ 191-number-of-1-bits/README.md
@@ -0,0 +1,6 @@
+# Number of 1 Bits
+
+Difficulty: Easy  
+Language: Java  
+Submitted: 2026-07-01T11:09:50.000Z  
+LeetCode: https://leetcode.com/problems/number-of-1-bits/
---
+++ 191-number-of-1-bits/number-of-1-bits.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int hammingWeight(int n) {
+        int count=0;
+        while(n>0){
+            int digit=(n&1);
+            if(digit==1){
+                count++;
+            }
+            n=n/2;
+        }
+        return count;
+    }
+}
