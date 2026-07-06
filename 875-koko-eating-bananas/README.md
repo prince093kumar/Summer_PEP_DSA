10d1c2550bb1d63b97bf462735142ab36a42253d
Prince kumar <122259133+prince093kumar@users.noreply.github.com>
2026-07-06 16:24:54 +0530

    875-koko-eating-bananas: accepted in Java | Time 25 ms | Memory 48.1 MB

---
+++ 875-koko-eating-bananas/README.md
@@ -0,0 +1,6 @@
+# Koko Eating Bananas
+
+Difficulty: Medium  
+Language: Java  
+Submitted: 2026-07-06T10:54:49.000Z  
+LeetCode: https://leetcode.com/problems/koko-eating-bananas/
---
+++ 875-koko-eating-bananas/koko-eating-bananas.java
@@ -0,0 +1,32 @@
+class Solution {
+
+    public boolean isPossible(int mid,int[] piles,int h){
+        int sum=0;
+        for(int i=0;i<piles.length;i++){
+            sum+=Math.ceil((double)piles[i]/mid);
+        }
+        return sum<=h;
+    }
+    public int minEatingSpeed(int[] piles, int h) {
+        int n=piles.length;
+        int max=piles[0];
+        for(int i=1;i<piles.length;i++){
+            if(piles[i]>max){
+                max=piles[i];
+            }
+        }
+        int low=1,high=max;
+        int ans=0;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(isPossible(mid,piles,h)){
+                ans=mid;
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        return ans;
+    }
+}
